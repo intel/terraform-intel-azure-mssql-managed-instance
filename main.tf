5187eb2087c0725a0e1e23956d7a2b3f9a919f97
Kinder Wischmeier <kinder.wischmeier@intel.com>
2023-04-04 09:51:19 -0400

    added to main and variables

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ data "azurerm_resource_group" "rg" {
 
 data "azurerm_virtual_network" "vnet" {
   name                = var.azurerm_virtual_network_name
-  resource_group_name = var.virtual_network_resource_group_name
+  resource_group_name = var.azurerm_resource_group_name
 }
 
 data "azurerm_subnet" "example" {
@@ -13,31 +13,34 @@ data "azurerm_subnet" "example" {
   resource_group_name  = data.azurerm_resource_group.rg.name
 }
 
+data "azurerm_network_security_group" "example" {
+  name                = var.nsg_name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
 
-resource "azurerm_sql_managed_instance" "example" {
-  name                = var.vm_name
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = var.azurerm_resource_group_name
-  administrator_login = var.administrator_login
-  sku_name            = var.sku_name
-  storage_size_gb     = var.storage_size_gb
-  vcore_count         = var.vcore_count
-  edition             = var.edition
-  hardware_family     = var.hardware_family
-  license_type        = var.license_type 
-  subnet_id           = var.subnet_id
+resource "azurerm_mssql_managed_instance" "example" {
+  name                         = var.vm_name
+  location                     = data.azurerm_resource_group.rg.location
+  resource_group_name          = var.azurerm_resource_group_name
+  administrator_login          = var.administrator_login
+  administrator_login_password = var.administrator_login_password
+  sku_name                     = var.sku_name
+  storage_size_in_gb           = var.storage_size_gb
+  vcores                       = var.vcore_count
+  license_type                 = var.license_type
+  subnet_id                    = data.azurerm_subnet.example.id
 }
 
 
-resource "azurerm_sql_database" "example" {
-  name                             = "my-database"
-  resource_group_name              = azurerm_resource_group.example.name
-  location                         = azurerm_resource_group.example.location
-  server_name                      = azurerm_sql_server.example.name
-  edition                          = "Standard"
-  collation                        = "SQL_Latin1_General_CP1_CI_AS"
-  max_size_bytes                   = 2147483648
-  requested_service_objective_name = "S0"
+# resource "azurerm_sql_database" "example" {
+#   name                             = "my-database"
+#   resource_group_name              = azurerm_resource_group.example.name
+#   location                         = data.azurerm_resource_group.rg.location
+#   server_name                      = azurerm_sql_server.example.name
+#   edition                          = "Standard"
+#   collation                        = "SQL_Latin1_General_CP1_CI_AS"
+#   max_size_bytes                   = 2147483648
+#   requested_service_objective_name = "S0"
 
-  tags                             = var.tags
-}
+#   tags = var.tags
+# }
--- variables.tf
+++ variables.tf
@@ -2,45 +2,38 @@
 ####     Intel      ####
 ########################
 variable "sku_name" {
-    description     = "The SKU that will be configured for the provisioned virtual machine"
-    type            = string
-    default         = "GP_Gen5_4"
+  description = "The SKU that will be configured for the provisioned virtual machine"
+  type        = string
+  default     = "GP_Gen5"
 }
 
 variable "storage_size_gb" {
-    description     = "The size of storage to be configured on the virtual machine"
-    type            = number
-    default         = 256
+  description = "The size of storage to be configured on the virtual machine"
+  type        = number
+  default     = 256
 }
 
 variable "vcore_count" {
-    description     = ""
-    type            = number
-    default         = 4
+  description = ""
+  type        = number
+  default     = 8
 }
 
 variable "edition" {
-    description     = ""
-    type            = string
-    default         = "GeneralPurpose"
+  description = ""
+  type        = string
+  default     = "GeneralPurpose"
 }
-
-variable "hardware_family" {
-    description     = ""
-    type            = string
-    default         = "Intel"
-}
-
 variable "license_type" {
-    description     = ""
-    type            = string
-    default         = "LicenseIncluded"
+  description = ""
+  type        = string
+  default     = "LicenseIncluded"
 }
-variable "subnet_id" {
-    description     = "The subnet resource id that the SQL Managed instance will be associated with."
-    type            = string
-    default         = "d4ab7583-eee6-45fe-9487-a7a0b59a389a"
-}
+# variable "subnet_id" {
+#   description = "The subnet resource id that the SQL Managed instance will be associated with."
+#   type        = string
+#   default     = null
+# }
 
 
 
@@ -49,53 +42,61 @@ variable "subnet_id" {
 ########################
 
 variable "administrator_login" {
-    description = "The administrator login name for the new server"
-    type        = string
+  description = "The administrator login name for the new server"
+  type        = string
+  default     = "_ccedbg"
 }
 
 variable "administrator_login_password" {
-    description = "The password associated with the administrator_login user"
-    type        = string
-    sensitive   = true
-    validation {
+  description = "The password associated with the administrator_login user"
+  type        = string
+  sensitive   = true
+  validation {
     condition     = length(var.administrator_login_password) >= 8
     error_message = "The administrator_login_password value must be at least 8 characters in length"
   }
 }
 
 variable "azurerm_resource_group_name" {
-    description = "Name of the resource group to be imported"
-    type = string
+  description = "Name of the resource group to be imported"
+  type        = string
+  default     = "kinder-testing"
 }
 
 variable "azurerm_virtual_network_name" {
-    description = "Name of the preconfigured virtual network"
-    type        = string
+  description = "Name of the preconfigured virtual network"
+  type        = string
+  default     = "kinder-testing"
 }
 
 variable "azurerm_subnet_name" {
   description = "The name of the preconfigured subnet"
   type        = string
+  default     = "default"
+
 }
 
-
-
+variable "nsg_name" {
+  description = ""
+  type        = string
+  default     = "kinder-testing"
+}
 
 ########################
 ####     Other      ####
 ########################
 
 variable "vm_name" {
-    description = "The unique name of the Linux virtual machine"
-    type = string
-    default = "vm1"
+  description = "The unique name of the Linux virtual machine"
+  type        = string
+  default     = "vm1"
 }
 
 variable "tags" {
-    description = "A mapping of tags to assign to the resource"
-    type        = map(any)
-    default = {
-        owner = "owner@company.com"
-        duration = "4"
+  description = "A mapping of tags to assign to the resource"
+  type        = map(any)
+  default = {
+    owner    = "owner@company.com"
+    duration = "4"
   }
 }
